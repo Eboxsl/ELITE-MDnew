eb89c52662f3f5421f408b49b9acbecebd874562
Eboxsl <144025781+Eboxsl@users.noreply.github.com>
2024-06-20 10:18:34 +0530

    By Elite md🇱🇰

--- commands/yt-playlist.js
+++ commands/yt-playlist.js
@@ -19,7 +19,7 @@ cmd({
                 return `${Math.floor(Math.random() * 10000)}${ext}`;
             };
             if (!text) {
-                citel.reply(`❌Please provide me a url first`);
+                citel.reply(`❌Please provide me a url `);
                 return;
             }
 let urlYtt = text.split('=')[1]
